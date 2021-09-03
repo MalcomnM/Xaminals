edcc25b39e9113cf3fbf2b53d6890aa147170bad
Malcomn <malcomn.j.mitchell@gmail.com>
2021-09-03 13:03:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Xaminals
+- - -
+Xaminals is a mobile app for zoophiles, people who are interested in learning about the origin and details of animals. Each tab view contains a list of animals that you can click through to view a details page.
+- - - 
+## Feature 1
+
+Users want a new tab that shows a list of Monkeys. When a user clicks the list they want to be navigated to a details page that displays a description of the animal.
+- Create a view that shows a list of Monkeys, that navigates 
+- Monkey icon: monkey.png
+
+![Xaminals application screenshot](Screenshots/03All.png "Xaminals application screenshot")
+![Xaminals application screenshot](Screenshots/08All.png "Xaminals application screenshot")
+
+---
+## Feature 2
+
+Users want to be able to search for animals by Name.
+
+In the AnimalSearchHandler implement the logic necessary to search by an animal's name.
+
+---
---
+++ Xaminals/AppShell.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xaminals.Views;
+
+namespace Xaminals
+{
+    public partial class AppShell : Shell
+    {
+        public Dictionary<string, Type> Routes { get; private set; } = new Dictionary<string, Type>();
+        public ICommand HelpCommand => new Command<string>(async (url) => await Launcher.OpenAsync(url));
+
+        public AppShell()
+        {
+            InitializeComponent();
+            RegisterRoutes();
+            BindingContext = this;
+        }
+
+        void RegisterRoutes()
+        {
+            Routes.Add("beardetails", typeof(BearDetailPage));
+            Routes.Add("catdetails", typeof(CatDetailPage));
+            Routes.Add("dogdetails", typeof(DogDetailPage));
+            Routes.Add("elephantdetails", typeof(ElephantDetailPage));
+
+            foreach (var item in Routes)
+            {
+                Routing.RegisterRoute(item.Key, item.Value);
+            }
+        }
+        
+    }
+}
